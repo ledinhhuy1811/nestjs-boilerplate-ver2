141d482b2a01b2f667d20cba701072f53f69a36a
ledinhhuy1811 <huygamo567@gmail.com>
2026-04-07 23:16:29 +0700

    feat: add security check for boostrap function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // set security http header
+  app.use(helmet());
+
+  // set cors
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  // gzip compression
+  app.use(compression());
+
+  // cookie parser
+  app.use(cookieParser());
+
+  // graceful shutdown
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 }
+
 bootstrap();
